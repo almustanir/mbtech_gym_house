9aa602c11629b9a51608f76bc14dbe7c8ec217a0
almustanir <olajidemubarak561@gmail.com>
2024-03-20 08:44:21 +0100

    searc

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p='20px'>
+      <Typography fontWeight={700} sx={{
+        fontSize: {lg: '44px', xs: '30px'}
+      }} mb="50px" textAlign="center">
+        Awesome Exercises You <br/> should know
+      </Typography>
+    </Stack>
+  )
+}
+
+export default SearchExercises
